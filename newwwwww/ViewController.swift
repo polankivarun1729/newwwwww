39da53661e1b9c1934d6926ca848872671805b18
abc <polankivarun1729@gmail.com>
2018-10-17 19:27:10 +0530

    Initial Commit

---
+++ newwwwww/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newwwwww
+//
+//  Created by Varun Kumar on 17/10/18.
+//  Copyright © 2018 Varun Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
